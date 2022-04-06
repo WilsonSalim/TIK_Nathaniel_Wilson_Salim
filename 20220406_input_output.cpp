585ce929fa3fe20fa9a6aa3e0365812b9b06b196
WilsonSalim <103086032+WilsonSalim@users.noreply.github.com>
2022-04-06 11:27:15 +0800

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {//begin of fucntion
+    string nama;
+    cout<<"Masukkan Nama: "; 
+    cin>>nama;
+    cout<<"Nama Anda: "<<nama;
+    
+    
+}//end of function
