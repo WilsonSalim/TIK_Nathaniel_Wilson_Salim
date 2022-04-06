34c1ac93744c0bfe00cf714904a4e60263cd0cf5
WilsonSalim <103086032+WilsonSalim@users.noreply.github.com>
2022-04-06 11:08:48 +0800

    Create 20220406_variabel.cpp

---
+++ 20220406_variabel.cpp
@@ -0,0 +1,13 @@
+//header
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {//begin of fucntion
+    float angka;
+    string nama; //deklarasi variabel
+    nama = "Nathaniel Wilson Salim"; //masukkan value kedalam variabel
+    angka = 10.21 + 3.14;
+    cout<<nama<<endl; //menampilkan variabel
+   cout<<angka;
+   
+}//end of function
