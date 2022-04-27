b002b3bee26a58e64047a2999c662cdf1fe998f3
WilsonSalim <103086032+WilsonSalim@users.noreply.github.com>
2022-04-27 10:15:56 +0800

    Create 20220427_while1.cpp

---
+++ 20220427_while1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a = 0;
+    while (a ++ <10){
+        cout<<"1 "<<" * "<<a<<" = "<<a*1<<endl;
+    }
+    cout<<endl<<endl;
+    int b = 0;
+    while (b ++ <10){
+        cout<<"2 "<<" * "<<b<<" = "<<b*2<<endl;
+    }
+    cout<<endl<<endl;
+    int c = 0;
+    while (c ++ <10){
+        cout<<"3 "<<" * "<<c<<" = "<<c*3<<endl;
+    }
+    cout<<endl<<endl;
+    int d = 0;
+    while (d ++ <10){
+        cout<<"4 "<<" * "<<d<<" = "<<d*4<<endl;
+    }
+    cout<<endl<<endl;
+    int e = 0;
+    while (e ++ <10){
+        cout<<"5 "<<" * "<<e<<" = "<<e*5<<endl;
+    }
+    cout<<endl<<endl;
+    int f = 0;
+    while (f ++ <10){
+        cout<<"6 "<<" * "<<f<<" = "<<f*6<<endl;
+    }
+    cout<<endl<<endl;
+    int g = 0;
+    while (g ++ <10){
+        cout<<"7 "<<" * "<<g<<" = "<<g*7<<endl;
+    }
+    cout<<endl<<endl;
+    int h = 0;
+    while (h ++ <10){
+        cout<<"8 "<<" * "<<h<<" = "<<h*8<<endl;
+    }
+    cout<<endl<<endl;
+    int i = 0;
+    while (i ++ <10){
+        cout<<"9 "<<" * "<<i<<" = "<<i*9<<endl;
+    }
+    cout<<endl<<endl;
+    int j = 0;
+    while (j ++ <10){
+        cout<<"10 "<<" * "<<j<<" = "<<j*10<<endl;
+    }
+    cout<<endl<<endl;
+    
+    
+    return 0;
+}
